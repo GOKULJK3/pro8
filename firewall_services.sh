0c2b1eae30c8482d46ed0555a2e969e308844d66
GOKUL_J <gokuljegan2@gmail.com>
2026-09-11 11:32:12 +0530

    Fix roll number in firewall_services.sh
    Updated roll number in comments.

--- firewall_services.sh
+++ firewall_services.sh
@@ -4,32 +4,26 @@
 # Managing Firewall Services using firewalld
 #
 # Student Name:gokul j
-# Roll Number:1u24it033
+# Roll Number:iu24it033
 # ==========================================
 
 # Check current firewall services
 sudo firewall-cmd --list-services
 
-
 # Allow HTTP service
 sudo firewall-cmd --permanent --add-service=http
-
 
 # Allow HTTPS service
 sudo firewall-cmd --permanent --add-service=https
 
-
 # Display enabled services
 sudo firewall-cmd --permanent --list-services
-
 
 # Remove HTTP service
 sudo firewall-cmd --permanent --remove-service=http
 
-
 # Allow SSH permanently
 sudo firewall-cmd --permanent --add-service=ssh
-
 
 # Reload firewall configuration
 sudo firewall-cmd --reload
